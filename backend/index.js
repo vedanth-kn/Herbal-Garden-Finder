d2d2d435a1d25a1f55deabc6d3897eed2eb6b03f
Vedanth K N <121715221+vedanth-kn@users.noreply.github.com>
2024-12-10 22:19:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors()); 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // MongoDB connection string
 const MONGO_URI = 'mongodb+srv://knvedanth:Varun9317@cluster0.vzc8z.mongodb.net/HerbalGardenDB?retryWrites=true&w=majority';
